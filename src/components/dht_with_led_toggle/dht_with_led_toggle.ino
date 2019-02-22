694d4680ff847221300481677afd8effc70d1b0b
Tyler Oalman <tyleroalman@gmail.com>
2019-02-22 15:29:10 -0600

    improve logic for demo

--- src/components/dht_with_led_toggle/dht_with_led_toggle.ino
+++ src/components/dht_with_led_toggle/dht_with_led_toggle.ino
@@ -62,6 +62,12 @@ void loop()
     Serial.println("%");
     Serial.print("Demo_var = ");
     Serial.println(demo_var, 1);
-    demo_var++;
+    
+    if (demo_var <= 20.0)
+      demo_var = 23.0;
+    else if (demo_var >= 26.0)
+      demo_var = 20.0;
+    else
+      demo_var++;
   }
 }
