2cd4688afbf221714a657b76943cee78ddcfc834
Tyler Oalman <tyleroalman@gmail.com>
2019-02-21 22:23:35 -0600

    automate toggle of led

--- src/components/dht_with_led_toggle/dht_with_led_toggle.ino
+++ src/components/dht_with_led_toggle/dht_with_led_toggle.ino
@@ -3,61 +3,65 @@
 #define LED 3
 
 static const int DHT_SENSOR_PIN = 2;
-DHT_nonblocking dht_sensor( DHT_SENSOR_PIN, DHT_SENSOR_TYPE );
+DHT_nonblocking dht_sensor(DHT_SENSOR_PIN, DHT_SENSOR_TYPE);
+float demo_var = 20.0;
 
 
-void setup( )
+void setup()
 {
-  Serial.begin( 9600);
+  Serial.begin(9600);
   pinMode(LED, OUTPUT);
   digitalWrite(LED, LOW);
 }
-
 
 
 /**
  *  Poll for a measurement, keeping the state machine alive.  Returns
  *  1true if a measurement is available.
  */
-static bool measure_environment( float *temperature, float *humidity )
+static bool measure_environment(float *temperature, float *humidity)
 {
-  static unsigned long measurement_timestamp = millis( );
+  static unsigned long measurement_timestamp = millis();
 
   /* Measure once every four seconds. */
-  if( millis( ) - measurement_timestamp > 3000ul )
+  if(millis() - measurement_timestamp > 3000ul)
   {
-    if( dht_sensor.measure( temperature, humidity ) == true )
+    if(dht_sensor.measure(temperature, humidity) == true)
     {
-      measurement_timestamp = millis( );
-      return( true );
+      measurement_timestamp = millis();
+      return(true);
     }
   }
 
-  return( false );
+  return(false);
 }
 
 
-
-void loop( )
+void loop()
 {
   float temperature;
   float humidity;
-
+  
   /* Measure temperature and humidity.  If true, measurement is available. */
-  if( measure_environment( &temperature, &humidity ) == true )
+  if(measure_environment(&temperature, &humidity) == true)
   {
-    if (temperature == 23.0)
+    if (demo_var >= 25.0)
     {
       digitalWrite(LED, HIGH);
     }
+
     else
     {
       digitalWrite(LED, LOW);
     }
-    Serial.print( "T = " );
-    Serial.print( temperature, 1 );
-    Serial.print( " deg. C, H = " );
-    Serial.print( humidity, 1 );
-    Serial.println( "%" );
+
+    Serial.print("T = ");
+    Serial.print(temperature, 1);
+    Serial.print(" deg. C, H = ");
+    Serial.print(humidity, 1);
+    Serial.println("%");
+    Serial.print("Demo_var = ");
+    Serial.println(demo_var, 1);
+    demo_var++;
   }
 }
