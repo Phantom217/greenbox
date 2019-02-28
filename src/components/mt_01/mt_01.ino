f29f51ac837b8d6c0a34162475395de3b26c327c
Tyler Oalman <tyleroalman@gmail.com>
2019-02-28 15:59:33 -0600

    add code using dht_nonblocking library

--- src/components/mt_01/mt_01.ino
+++ src/components/mt_01/mt_01.ino
@@ -1,35 +1,40 @@
-#include <dht.h>
+#include <dht_nonblocking.h>
+#define MT_SENSOR_TYPE DHT_TYPE_11
+
 static const int MT_01_PIN = A0;
+DHT_nonblocking mt_sensor(MT_01_PIN, MT_SENSOR_TYPE)
 
 void setup()
 {
-    Serial.begin(9600);
+  Serial.begin(9600);
 }
 
-static bool measure_soil_moisture(float moisture)
+static bool measure_soil_moisture(float *moisture)
 {
-    static unsigned long measurement_timestamp = millis();
+  static unsigned long measurement_timestamp = millis();
 
-    /* Measure once every four seconds */
-    if(millis() - measurement_timestamp > 3000ul)
-    {
-        if(mt_sensor.measure(moisture) == true)
-        {
-            measurement_timestamp = millis();
-            return(true);
-        }
-    }
+  /* Measure once every four seconds */
+  if(millis() - measurement_timestamp > 3000ul)
+  {
+      if(mt_sensor.measure(moisture) == true)
+      {
+          measurement_timestamp = millis();
+          return(true);
+      }
+  }
 
-    return(false);
+  return(false);
 }
 void loop()
 {
-    float moisture;
-    moisture = analogRead(MT_01_PIN);
-    moisture = map(moisture, 550, 0, 0, 100);
+  float moisture;
+  moisture = analogRead(MT_01_PIN);
+  moisture = map(moisture, 550, 0, 0, 100);
 
-    Serial.print("Moisture : ");
+  if(measure_soil_moisture(&moisture) == true)
+  {
+    Serial.print("Moisture = ");
     Serial.print(moisture);
     Serial.println("%");
-    delay(1000);
+  }
 }
