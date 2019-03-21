fb4aff732c88ef665a0248f4caec273d8f869329
Tyler Oalman <tyleroalman@gmail.com>
2019-03-21 15:27:39 -0500

    add lamp and fan control with timers

--- src/demo.ino
+++ src/demo.ino
@@ -1,17 +1,35 @@
 #include <dht_nonblocking.h>
 #define DHT_SENSOR_TYPE DHT_TYPE_11
-#define LED 3
 
-static const int DHT_SENSOR_PIN = 2;
+static const int LAMP_GREEN_PIN = 2;
+static const int LAMP_RED_PIN =  3;
+static const int FAN_01_PIN = 4;
+static const int DHT_SENSOR_PIN = 5;
 DHT_nonblocking dht_sensor(DHT_SENSOR_PIN, DHT_SENSOR_TYPE);
-float demo_var = 20.0;
-
+static const unsigned long LAMP_TIMER = 10000;
+unsigned long timer_start_lamp = 0;
+bool green_on = false;
+bool red_on = false;
+bool fan_on = false;
+bool timer_active_lamp = false;
 
 void setup()
 {
   Serial.begin(9600);
-  pinMode(LED, OUTPUT);
-  digitalWrite(LED, LOW);
+
+  pinMode(LAMP_GREEN_PIN, OUTPUT);
+  pinMode(LAMP_RED_PIN, OUTPUT);
+  pinMode(FAN_01_PIN, OUTPUT);
+
+  digitalWrite(LAMP_GREEN_PIN, LOW);
+  green_on = false;
+  digitalWrite(LAMP_RED_PIN, LOW);
+  red_on = false;
+  digitalWrite(FAN_01_PIN, LOW);
+  fan_on = false;
+
+  timer_start_lamp = millis();
+  timer_active_lamp = true;
 }
 
 
@@ -24,9 +42,9 @@ static bool measure_environment(float *temperature, float *humidity)
   static unsigned long measurement_timestamp = millis();
 
   /* Measure once every four seconds. */
-  if(millis() - measurement_timestamp > 3000ul)
+  if (millis() - measurement_timestamp > 3000ul)
   {
-    if(dht_sensor.measure(temperature, humidity) == true)
+    if (dht_sensor.measure(temperature, humidity) == true)
     {
       measurement_timestamp = millis();
       return(true);
@@ -43,16 +61,20 @@ void loop()
   float humidity;
 
   /* Measure temperature and humidity.  If true, measurement is available. */
-  if(measure_environment(&temperature, &humidity) == true)
+  if (measure_environment(&temperature, &humidity) == true)
   {
-    if (demo_var >= 25.0)
+    if (temperature >= 25.0)
     {
-      digitalWrite(LED, HIGH);
+      digitalWrite(FAN_01_PIN, HIGH);
+      fan_on = true;
+      Serial.println("Fan ON");
     }
 
     else
     {
-      digitalWrite(LED, LOW);
+      digitalWrite(FAN_01_PIN, LOW);
+      fan_on = false;
+      Serial.println("Fan OFF");
     }
 
     Serial.print("T = ");
@@ -60,14 +82,31 @@ void loop()
     Serial.print(" deg. C, H = ");
     Serial.print(humidity, 1);
     Serial.println("%");
-    Serial.print("Demo_var = ");
-    Serial.println(demo_var, 1);
+  }
 
-    if (demo_var <= 20.0)
-      demo_var = 23.0;
-    else if (demo_var >= 26.0)
-      demo_var = 20.0;
+  if (timer_active_lamp && (millis() - timer_start_lamp) >= LAMP_TIMER)
+  {
+    timer_start_lamp += LAMP_TIMER;
+    green_on = !green_on;
+    red_on = !red_on;
+
+    if (green_on && !red_on)
+    {
+      digitalWrite(LAMP_GREEN_PIN, HIGH);
+      digitalWrite(LAMP_RED_PIN, LOW);
+      Serial.println("Green Lamp ON, Heat Lamp OFF")
+    }
+
+    else if (red_on && !green_on)
+    {
+      digitalWrite(LAMP_RED_PIN, HIGH);
+      digitalWrite(LAMP_GREEN_PIN, LOW);
+      Serial.println("Heat Lamp ON, Green Lamp OFF")
+    }
+
     else
-      demo_var++;
+    {
+      Serial.println("Well, this is awkward...");
+    }
   }
 }
