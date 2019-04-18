f63a3708d4b068c631f1e100e39695301e7cfc9a
Tyler Oalman <tyleroalman@gmail.com>
2019-04-17 20:52:00 -0500

    add code for the ethernet shield

--- src/demo.ino
+++ src/demo.ino
@@ -4,45 +4,38 @@
 #include <Ethernet.h>
 #include <SD.h>
 
-/* static const int LAMP_GREEN_PIN = 2; */
 static const int LAMP_HEAT_PIN =  3;
 static const int FAN_01_PIN = 4;
 static const int DHT_SENSOR_PIN = 5;
 DHT_nonblocking dht_sensor(DHT_SENSOR_PIN, DHT_SENSOR_TYPE);
-/* static const unsigned long LAMP_TIMER = 10000; */
-/* unsigned long lamp_timer_start = 0; */
-/* bool green_on = false; */
-bool heat_on = false;
-bool fan_on = false;
-/* bool lamp_timer_active = false; */
+String STATUS_HEAT = "Heat OFF";
+String STATUS_FAN = "Fan OFF";
 
 byte mac[] = { 0x00, 0xAA, 0xBB,0xCC, 0xDE, 0x02 };
 
-char server_name[] = "web.greenbox.net";
+IPAddress ip(192.168.0.254);
 
-EthernetClient client;
+EthernetServer server(80);
 
 void setup()
 {
-  if (Ethernet.begin(mac) == 0)
-  {
-    Serial.println("Failed to configure Ethernet using DHCP");
-    while (true);
-  }
-
   Serial.begin(9600);
 
-/*   pinMode(LAMP_GREEN_PIN, OUTPUT); */
+  while (!Serial)
+  {
+    ;
+  }
+
+  Ethernet.begin(mac, ip);
+  server.begin();
+  Serial.print("server located at: ");
+  Serial.println(Ethernet.localIP());
+
   pinMode(LAMP_HEAT_PIN, OUTPUT);
   pinMode(FAN_01_PIN, OUTPUT);
 
-/*   digitalWrite(LAMP_GREEN_PIN, LOW); /* somehow this turns it on, not sure why */ */
-/*   green_on = true; */
   digitalWrite(LAMP_HEAT_PIN, HIGH); /* turns the lamp off */
-  heat_on = false;
-  digitalWrite(FAN_01_PIN, HIGH); /* same case as the above two */
-  fan_on = false;
-
+  digitalWrite(FAN_01_PIN, HIGH); /* same case as the above */
 }
 
 
@@ -82,25 +75,25 @@ void loop()
     if (temperature >= 32.0)
     {
       digitalWrite(FAN_01_PIN, LOW); /*turn fan on */
-      fan_on = true; /* set flag */
+      STATUS_FAN = "Fan ON";
       Serial.println("Fan ON");
     }
     if (temperature < 29.4)
     {
       digitalWrite(FAN_01_PIN, HIGH); /*turn fan off */
-      fan_on = false;
+      STATUS_FAN = "Fan OFF";
       Serial.println("Fan OFF");
     }
     if (temperature > 29.4)
     {
       digitalWrite(LAMP_HEAT_PIN, HIGH); /* heat lamp off */
-      heat_on = false;
+      STATUS_HEAT = "Heat OFF";
       Serial.println("Heat OFF: Temp > 85 deg. F");
     }
     if (tempreature <= 26.6)
     {
       digitalWrite(LAMP_HEAT_PIN, LOW); /* heat lamp on */
-      heat_on = true;
+      STATUS_HEAT = "Heat ON";
       Serial.println("Heat ON: Temp < 80 deg. F");
     }
 
@@ -111,36 +104,54 @@ void loop()
     Serial.println("%");
   }
 
-
-}
-
-void sendGET()
-{
-  if (client.connect(server_name, 80)) /* start client connection, check for connection*/
+ EthernetClient client = server.available();
+  if (client)
   {
-    Serial.println("connected");
-/* edit next 2 lines for reading html from sd card */
-    client.println("GET /~shb/arduino.txt HTTP/1.1"); /* download text */
-    client.println("Host: web.greenbox.net");
-    client.println("Connection: close"); /* close 1.1 persistent connection */
-    client.println(); /* end of get request */
-  }
-  else
-  {
-    Serial.println("connection failed"); /* error message if no client */
-    Serial.println();
-  }
+    Serial.println("new client");
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected())
+    {
+      if (client.available())
+      {
+        char c = client.read();
+        Serial.write(c);
+        if (c == '\n' && currentLineIsBlank)
+        {
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println("Connection: close");  // the connection will be closed after completion of the response
+          client.println("Refresh: 5");  // refresh the page automatically every 5 sec
+          client.println();
+          client.println("<!DOCTYPE HTML>");
+          client.println("<html>");
 
-  while (client.connected() && !client.available()) delay(1); /* wait for data */
-  while (client.connected() || !client.available()) /* connected or data available */
-  {
-    char c = client.read(); /* get byte from ethernet buffer */
-    Serial.print(c); /* print byte to serial monitor */
-  }
+          client.print("T = ");
+          client.print(temp_f, 1);
+          client.print(" deg. F, H = ");
+          client.print(humidity, 1);
+          client.println("%");
 
-  Serial.println();
-  Serial.println("disconnecting...");
-  Serial.println("=====================");
-  Serial.println();
-  client.stop(); /* stop client */
+          client.println("<br>");
+          client.print(STATUS_HEAT);
+          client.print("----");
+          client.print(STATUS_FAN);
+
+          client.println("</html>");
+          break;
+        }
+        if (c == '\n')
+        {
+          currentLineIsBlank = true;
+        }
+        else if (c != '\r')
+        {
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    delay(1);
+    client.stop();
+    Serial.println("client disconnected");
+  }
 }
