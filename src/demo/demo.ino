c2d547daffbcc1a199d1c5e9dac2cb3f7aeefd37
Tyler Oalman <tyleroalman@gmail.com>
2019-04-22 19:33:19 -0500

    add parameters for soil sensor

--- src/demo/demo.ino
+++ src/demo/demo.ino
@@ -1,4 +1,3 @@
-#include <dht.h>
 #include <dht_nonblocking.h>
 #define DHT_SENSOR_TYPE DHT_TYPE_11
 #include <SPI.h>
@@ -7,10 +6,16 @@
 static const int LAMP_HEAT_PIN =  3;
 static const int FAN_01_PIN = 4;
 static const int DHT_SENSOR_PIN = 5;
-static const int MT_01_PIN = A0;
+static const int SOIL_PIN = A0;
+int moisture_val = 0;
+
 DHT_nonblocking dht_sensor(DHT_SENSOR_PIN, DHT_SENSOR_TYPE);
+
 String STATUS_HEAT = "Heat OFF";
 String STATUS_FAN = "Fan OFF";
+String STATUS_SOIL = "Soil: Too Dry";
+
+boolean moist_soil = false;
 
 byte mac[] = { 0x00, 0xAA, 0xBB,0xCC, 0xDE, 0x02 };
 
@@ -64,49 +69,61 @@ static bool measure_environment(float *temperature, float *humidity)
 void loop()
 {
   float temperature;
-  float temp_f;
   float humidity;
-  float moisture;
-
-  temp_f = ((temperature * 9/5) + 32);
-  moisture = analogRead(MT_01_PIN);
-  moisture = map(moisture, 550, 0, 0, 100);
 
   /* Measure temperature and humidity.  If true, measurement is available. */
   if (measure_environment(&temperature, &humidity) == true)
   {
-    if (temperature >= 32.0)
+    if (((temperature*9/5)+32) >= 90.0)
     {
       digitalWrite(FAN_01_PIN, LOW); /*turn fan on */
       STATUS_FAN = "Fan ON";
       Serial.println("Fan ON");
     }
-    if (temperature < 29.4)
+    if (((temperature*9/5)+32) < 85.0)
     {
       digitalWrite(FAN_01_PIN, HIGH); /*turn fan off */
       STATUS_FAN = "Fan OFF";
       Serial.println("Fan OFF");
     }
-    if (temperature > 29.4)
+    if (((temperature*9/5)+32) > 85.0)
     {
       digitalWrite(LAMP_HEAT_PIN, HIGH); /* heat lamp off */
       STATUS_HEAT = "Heat OFF";
       Serial.println("Heat OFF: Temp > 85 deg. F");
     }
-    if (temperature <= 26.6)
+    if (((temperature*9/5)+32) <= 80.0)
     {
       digitalWrite(LAMP_HEAT_PIN, LOW); /* heat lamp on */
       STATUS_HEAT = "Heat ON";
       Serial.println("Heat ON: Temp < 80 deg. F");
     }
 
+    moisture_val = analogRead(SOIL_PIN);
+
+    if (moisture_val > 350 && moisture_val < 700)
+    {
+      moist_soil = true;
+      STATUS_SOIL = "Soil: Okay";
+    }
+    else if (moisture_val > 700)
+    {
+      moist_soil = false;
+      STATUS_SOIL = "Soil: Too Dry";
+    }
+    else
+    {
+      moist_soil = false;
+      STATUS_SOIL = "Soil: Too Much Water";
+    }
+
     Serial.print("T = ");
-    Serial.print(temp_f, 1);
+    Serial.print(((temperature*9/5)+32), 1);
     Serial.print(" deg. F, H = ");
     Serial.print(humidity, 1);
     Serial.print("%, Moisture = ");
-    Serial.print(moisture, 1);
-    Serial.println("%");
+    Serial.println(moisture_val, 1);
+    Serial.println(STATUS_SOIL);
   }
 
  EthernetClient client = server.available();
@@ -126,18 +143,17 @@ void loop()
           client.println("HTTP/1.1 200 OK");
           client.println("Content-Type: text/html");
           client.println("Connection: close");  // the connection will be closed after completion of the response
-          client.println("Refresh: 5");  // refresh the page automatically every 5 sec
+          client.println("Refresh: 4");  // refresh the page automatically every 5 sec
           client.println();
           client.println("<!DOCTYPE HTML>");
           client.println("<html>");
 
           client.print("T = ");
-          client.print(temp_f, 1);
+          client.print(((temperature*9/5)+32), 1);
           client.print(" deg. F, H = ");
           client.print(humidity, 1);
-          client.print("%, Moisture = ");
-          client.print(moisture, 1);
-          client.println("%");
+          client.print("%, ");
+          client.println(STATUS_SOIL);
 
           client.println("<br>");
           client.print(STATUS_HEAT);
